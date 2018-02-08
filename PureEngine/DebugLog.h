e21a51a767508d0d2d65e83c2c68932679a79f16
Matthew McDade <Matthew.McDade13@gmail.com>
2018-02-07 23:00:24 -0800

    Added Vector2 and Printable overloads for print function for debug logging

--- PureEngine/DebugLog.h
+++ PureEngine/DebugLog.h
@@ -1,18 +1,23 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <SFML/System/Vector2.hpp>
+#include "Printable.h"
+#include "VecToString.h"
 
 namespace pure
 {
 	// prints given arg to stdout
 	template <typename T>
-	void print(const T& s)
-	{
-		std::cout << s;
-	}
+	void print(const T& s) { std::cout << s; }
+
+	template<typename T>
+	void print(const sf::Vector2<T>& vec) { std::cout << (vecToString(vec)); }
+
+	void print(const Printable& obj) { std::cout << (obj.toString()); }
 
 	// sends newline and flushes stdout
-	void cLog() { std::cout << std::endl; }
+	inline void cLog() { std::cout << std::endl; }
 
 	// prints all args to stdout and then flushes it
 	template <typename T, typename... Args>
