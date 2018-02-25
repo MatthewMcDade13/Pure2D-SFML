b2095daf1f532374ffb76542c294998e643fe50a
MatthewMcDade13 <Matthew.McDade13@gmail.com>
2018-02-25 04:23:42 -0800

    Removed binaries and unecessary generated cmake files from source control

--- PureEngine/pureengine-config.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(PUREENGINE_CMAKE_DIR "/media/matt/Games/Linux/Projects/VSCode/Asteroids/PureEngineLib/PureEngine/CMakeLists.txt" PATH)
-set(PUREENGINE_INCLUDE_DIRS "")
-
-set(PUREENGINE_LIBRARIES PureEngine)
